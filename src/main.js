f8feefee29693886d375d20588d3ed447bf79ec6
linyongzuo <414747746@qq.com>
2023-05-15 16:33:06 +0800

    first init

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import router from './router'
+import store from './store'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+createApp(App).use(store).use(router).use(Antd).mount('#app')
